985b0eb75a0a8303b887f769176cb7eef3255b18
Patrick Maloney <maloneypatr@gmail.com>
2014-02-09 08:57:49 -0500

    commit 8

--- R/dedollar.R
+++ R/dedollar.R
@@ -1,8 +1,26 @@
-dedollar <- function(x, fillblanks = NULL, convert = FALSE){
+#' Remove the dollar sign from a vector
+#' 
+#' Remove the dollar sign from a vector
+#' 
+#' @param x a vector
+#' @param convert logical.  Defaults to false.  If true, vector is converted to numeric.
+#' 
+#' @return A vector of length(x) with the dollar sign removed
+#' 
+#' @examples 
+#' x <- c('$3.44', '$28,339')
+#' dedollar(x)
+#' [1] '3.44' '29,339'
+#' 
+#' y <- C('$3.44', '$42.33')
+#' dedollar(y, convert = T)
+#' [1] 3.44   42.33
+#' 
+#' @export
+dedollar <- function(x, convert = FALSE){
   require(stringr)
-  xchar <- str_replace(x, '\\$', '')
-  xfill <- ifelse(xchar == '', fillblanks, xchar)
+  x <- str_replace(x, '\\$', '')
   if(convert)
-    return(as.numeric(xfill)) else 
-      xfill
+    return(as.numeric(x)) else 
+      x
 }
