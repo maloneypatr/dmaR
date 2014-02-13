fea943852a70f89d5f320c60837a5543cb1db080
Patrick Maloney <maloneypatr@gmail.com>
2014-02-13 11:29:32 -0500

    commit 18

--- R/vbr.R
+++ R/vbr.R
@@ -1,14 +1,21 @@
-vbr <- function(df, dim = NULL, spend = NULL, metric = NULL, avg = T){
-    df <- df
-    dim <- eval(substitute(df$dim))
-    spend <- eval(substitute(df$spend))
-    metric <- eval(substitute(df$metric))
+vbr <- function(df = NULL, spend = NULL, metric = NULL, avg = T){
+    if(is.null(df)){
+      spend = spend 
+      metric = metric
+    } else {
+      df <- df
+      spend <- eval(substitute(df$spend))
+      metric <- eval(substitute(df$metric))
+    }
     spend_wo_dim <- sum(spend) - spend
     metric_wo_dim <- sum(metric) - metric
     ratio_wo_dim <- spend_wo_dim/metric_wo_dim
     vbr <- ratio_wo_dim - (sum(spend)/sum(metric))
     average <- sum(spend)/sum(metric)
     avg_vbr <- average + vbr
-    answer <- ifelse(!avg, vbr, avg_vbr)
+    if(!avg) 
+      answer <- vbr
+    else 
+      answer <- avg_vbr
     return(answer)
 }
