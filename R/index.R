0d90e987ecfa154d5492da7a71020ce194ebaae6
Patrick Maloney <maloneypatr@gmail.com>
2014-04-10 10:27:45 -0400

    added inverse index for cost per functions

--- R/index.R
+++ R/index.R
@@ -1,7 +1,19 @@
-index <- function(x, base = 'mean'){
+index <- function(x, base = 'mean', inv = F){
+  
   xMean <- mean(x)
   xMedian <- median(x)
-  if(base == 'median')
-    return(x/xMedian*100) else
-      (return(x/xMean*100))
+  
+  if(inv){
+    
+    if(base == 'median')
+      return(xMedian/x*100) else
+        (return(xMean/x*100))
+    
+  } else {
+    
+    if(base == 'median')
+      return(x/xMedian*100) else
+        (return(x/xMean*100))
+  
+  }
 }
